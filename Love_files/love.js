8a9e83f5f2ee0d75617c11a218e00b74013c9d21
cecilxp <122683834+cecilxp@users.noreply.github.com>
2024-05-19 21:01:56 +0200

    Update love.js

--- Love_files/love.js
+++ Love_files/love.js
@@ -147,25 +147,6 @@ function Heart(numPoints = 5, innerRadius = 5, outerRadius = 10) {
         }
     }
 
-    Star = function() {
-        // x = 16 sin^3 t
-        // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
-        // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
-        var points = [], x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
-            t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
-            points.push(new Point(x, y));
-        }
-        this.points = points;
-        this.length = points.length;
-    }
-    Star.prototype = {
-        get: function(i, scale) {
-            return this.points[i].mul(scale || 1);
-        }
-    }
     Seed = function(tree, point, scale, color) {
         this.tree = tree;
 
