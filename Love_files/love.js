4924f1742e63d853b0b16710e2d8e911edb4d1d5
cecilxp <122683834+cecilxp@users.noreply.github.com>
2024-05-19 21:04:39 +0200

    Update love.js

--- Love_files/love.js
+++ Love_files/love.js
@@ -71,25 +71,7 @@ function Heart(numPoints = 5, innerRadius = 5, outerRadius = 10) {
             return this.points[i].mul(scale || 1);
         }
     }
-    Star = function() {
-        // x = 16 sin^3 t
-        // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
-        // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
-        var points = [], x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
-            t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
-            points.push(new Point(x, y));
-        }
-        this.points = points;
-        this.length = points.length;
-    }
-    Star.prototype = {
-        get: function(i, scale) {
-            return this.points[i].mul(scale || 1);
-        }
-    }
+
 (function(window){
 
     function random(min, max) {
@@ -153,11 +135,11 @@ function Heart(numPoints = 5, innerRadius = 5, outerRadius = 10) {
         var scale = scale || 1
         var color = color || '#FFFFFF';
 
-        this.star = {
+        this.heart = {
             point  : point,
             scale  : scale,
             color  : color,
-            figure : new Star(),
+            figure : new Heart(),
         }
 
         this.cirle = {
