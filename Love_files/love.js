bb54634ce81c343c5902aeda944a34df0e1f8689
cecilxp <122683834+cecilxp@users.noreply.github.com>
2024-05-19 20:11:09 +0200

    Update love.js

--- Love_files/love.js
+++ Love_files/love.js
@@ -53,16 +53,18 @@
     }
 
     Heart = function() {
-        // x = 16 sin^3 t
-        // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
-        // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
-        var points = [], x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
-            t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
-            points.push(new Point(x, y));
-        }
+            var points = [], x, y, angle;
+            var numPoints = 5;
+            var innerRadius = 5;
+            var outerRadius = 10;
+            
+            for (var i = 0; i < numPoints * 2; i++) {
+                angle = Math.PI / numPoints * i;
+                var radius = (i % 2 === 0) ? outerRadius : innerRadius;
+                x = radius * Math.cos(angle);
+                y = radius * Math.sin(angle);
+                points.push(new Point(x, y));
+            }
         this.points = points;
         this.length = points.length;
     }
